7afb225eca57ef55a8db39d35abd25e428e805e6
cjmunozy <cjmunoz@espol.edu.ec>
2024-07-28 19:59:13 -0500

    Integración con Swagger añadida
    Se integró la API con Swagger y se usó Postman para hacer pruebas de inserción, actualización y borrado en la base de datos

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,6 +2,23 @@ const admin = require('firebase-admin');
 const db = admin.firestore();
 
 exports.createItem = async (req, res) => {
+    /* 
+    #swagger.tags = ['Items']
+    #swagger.description = 'Create an item'
+    #swagger.summary = 'Create an item'
+    #swagger.parameters['data'] = {
+        in: 'body',
+        description: 'Data to create an item',
+        required: true,
+    }
+    #swagger.responses[201] = {
+        description: 'Item successfully created',
+    }
+    #swagger.responses[400] = {
+        description: 'Bad request',
+    }
+    */
+
     try {
         const data = req.body;
         const itemRef = await db.collection('items').add(data);
@@ -12,6 +29,18 @@ exports.createItem = async (req, res) => {
 };
 
 exports.getAllItems = async (req, res) => {
+    /* 
+    #swagger.tags = ['Items']
+    #swagger.description = 'Get all items entries'
+    #swagger.summary = 'Get all items entries'
+    #swagger.responses[200] = {
+        description: 'Items entries successfully obtained',
+    }
+    #swagger.responses[400] = {
+        description: 'Bad request',
+    }
+    */
+
     try {
         const itemsSnapshot = await db.collection('items').get();
         const items = [];
@@ -23,6 +52,25 @@ exports.getAllItems = async (req, res) => {
 };
 
 exports.getItem = async (req, res) => {
+    /* 
+    #swagger.tags = ['Items']
+    #swagger.description = 'Get an item entry'
+    #swagger.summary = 'Get an item entry'
+    #swagger.parameters['id'] = {
+        description: 'Item id',
+        required: true,
+    }
+    #swagger.responses[404] = {
+        description: 'Item not found',
+    }
+    #swagger.responses[400] = {
+        description: 'Bad request',
+    }
+    #swagger.responses[200] = {
+        description: 'Get an item by id',
+    }
+    */
+    
     try {
         const itemId = req.params.id;
         const itemDoc = await db.collection('items').doc(itemId).get();
@@ -37,6 +85,30 @@ exports.getItem = async (req, res) => {
 }
 
 exports.updateItem = async (req, res) => {
+    /* 
+    #swagger.tags = ['Items']
+    #swagger.description = 'Update an item entry'
+    #swagger.summary = 'Update an item entry'
+    #swagger.parameters['id'] = {
+        description: 'Item id',
+        required: true,
+    }
+    #swagger.parameters['data'] = {
+        in: 'body',
+        description: '',
+        required: true,
+    }
+    #swagger.responses[404] = {
+        description: 'Item not found',
+    }
+    #swagger.responses[400] = {
+        description: 'Bad request',
+    }
+    #swagger.responses[200] = {
+        description: 'Item entry succesfully updated',
+    }
+    */
+
     try {
         const itemId = req.params.id;
         const data = req.body;
@@ -49,6 +121,25 @@ exports.updateItem = async (req, res) => {
 }
 
 exports.deleteItem = async (req, res) => {
+    /* 
+    #swagger.tags = ['Items']
+    #swagger.description = 'Delete an item entry'
+    #swagger.summary = 'Delete an item entry'
+    #swagger.parameters['id'] = {
+        description: 'Item id',
+        required: true,
+    }
+    #swagger.responses[404] = {
+        description: 'Item not found',
+    }
+    #swagger.responses[400] = {
+        description: 'Bad request',
+    }
+    #swagger.responses[200] = {
+        description: 'Item entry succesfully deleted',
+    }
+    */
+
     try {
         const itemId = req.params.id;
         await db.collection('items').doc(itemId).delete()
